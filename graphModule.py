6a57b813c0bd966403cc4c1acf620f127f66f1bd
CUGGISer <47958541+CUGGISer@users.noreply.github.com>
2022-02-08 15:24:09 +0800

    Update graphModule.py

--- graphModule.py
+++ graphModule.py
@@ -8,10 +8,10 @@ class GraphConv2(nn.Module):
     def __init__(self, batch = 1, h=[16,32,64,128,256], w=[16,32,64,128,256], d=[768,512], V=[2,4,8,32],outfeatures=[64,32]):
         super(GraphConv2, self).__init__()
 
-        self.gc1 = GCU(batch = batch, h=h[0], w=w[0], d=d[0], V=V[0],outfeatures=outfeatures[0])
-        self.gc2 = GCU(batch = batch, h=h[0], w=w[0], d=d[0], V=V[1],outfeatures=outfeatures[0])
+        #self.gc1 = GCU(batch = batch, h=h[0], w=w[0], d=d[0], V=V[0],outfeatures=outfeatures[0])
+        #self.gc2 = GCU(batch = batch, h=h[0], w=w[0], d=d[0], V=V[1],outfeatures=outfeatures[0])
         self.gc3 = GCU(batch = batch, h=h[0], w=w[0], d=d[0], V=V[1],outfeatures=outfeatures[0])
-        self.gc4 = GCU(batch = batch, h=h[0], w=w[0], d=d[0], V=V[3],outfeatures=outfeatures[0])
+        #self.gc4 = GCU(batch = batch, h=h[0], w=w[0], d=d[0], V=V[3],outfeatures=outfeatures[0])
       
     def forward(self, x):
         
